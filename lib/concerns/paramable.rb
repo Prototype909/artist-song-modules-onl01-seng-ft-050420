057504ab17eaeb27f7a01522150bb54cf55f7dd7
Steve Anderson <prototype909@msn.com>
2020-05-22 20:32:02 +0000

    Automatically backed up by Learn

--- lib/concerns/paramable.rb
+++ lib/concerns/paramable.rb
@@ -1,14 +1,14 @@
 module Paramable
-<<<<<<< HEAD
+
   def to_param
     name.downcase.gsub(' ', '-')
   end
 end
-=======
+
 
   def to_param
      name.downcase.gsub(' ', '-')
   end
 
 end 
->>>>>>> 3cd43f68a7fea9b1e7532fca9c6b1af69fc1779c
+
