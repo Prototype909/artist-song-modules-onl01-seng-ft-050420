7d3640f9a9727a6eba8e460588110bf19bd52853
Steve Anderson <prototype909@msn.com>
2020-05-22 20:25:33 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,9 @@
 module Findable
-<<<<<<< HEAD
+
   def find_by_name(name)
     all.detect{|a| a.name == name}
   end
-=======
+
 
   module ClassMethods
 
@@ -12,5 +12,5 @@ module Findable
     end
 
 
->>>>>>> 3cd43f68a7fea9b1e7532fca9c6b1af69fc1779c
+
 end
